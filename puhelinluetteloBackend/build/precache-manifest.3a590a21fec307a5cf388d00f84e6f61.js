26f7f77dbbb87da85c9d612a36f1018ad454f8eb
Tenukki <tenhunens@hotmail.com>
2019-04-15 20:23:21 +0300

    tokacommit

---
+++ puhelinluetteloBackend/build/precache-manifest.3a590a21fec307a5cf388d00f84e6f61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8208047b89611e6d7b88",
+    "url": "/static/css/main.4374fd54.chunk.css"
+  },
+  {
+    "revision": "8208047b89611e6d7b88",
+    "url": "/static/js/main.92d30365.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "37cfae212e25180f959b",
+    "url": "/static/js/2.0b74813f.chunk.js"
+  },
+  {
+    "revision": "a1e3397401d2ff82b0db8d4cb1bdcf18",
+    "url": "/index.html"
+  }
+];
