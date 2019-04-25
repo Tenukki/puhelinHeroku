7e75685348c74dfc37ed81cf258fd8f893c90424
Tenukki <tenhunens@hotmail.com>
2019-04-25 21:47:35 +0300

    ada

---
+++ puhelinluetteloBackend/build/precache-manifest.ed82a56a1f9bc57c2d36e2c9604f2975.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "388e6e44fbbdf61d07c4",
+    "url": "/static/css/main.19ebd505.chunk.css"
+  },
+  {
+    "revision": "388e6e44fbbdf61d07c4",
+    "url": "/static/js/main.bd8b4b2b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "37cfae212e25180f959b",
+    "url": "/static/js/2.0b74813f.chunk.js"
+  },
+  {
+    "revision": "b1937a3cb168df2f8ecfefe92a40cb6d",
+    "url": "/index.html"
+  }
+];
